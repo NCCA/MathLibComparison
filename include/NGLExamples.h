a5aa53992e116ded7a44c4471c2b91e618f69723
jmacey <jmacey@bournemouth.ac.uk>
2015-09-14 17:16:26 +0100

    updated

---
+++ include/NGLExamples.h
@@ -0,0 +1,12 @@
+#ifndef NGLEXAMPLES_H__
+#define NGLEXAMPLES_H__
+
+namespace NGLExamples
+{
+	extern void initIdentity();
+	extern void print();
+	extern void scale(float _x, float _y, float _z);
+	extern void translate(float _x, float _y, float _z);
+	extern void printOpenGL();
+}
+#endif
